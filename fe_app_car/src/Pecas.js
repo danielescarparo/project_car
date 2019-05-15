017d3d483080de8125afee1b81fa1acc2ca2010f
Daniele <daniele.scarparo@dextra-sw.com>
2019-05-15 12:00:44 -0300

    informações das peças com avisos

--- fe_app_car/src/Pecas.js
+++ fe_app_car/src/Pecas.js
@@ -8,7 +8,7 @@ class Pecas extends Component {
   };
 
   componentDidMount(){
-    axios.get(`http://private-0a4c11-projectcar.apiary-mock.com/carros/${this.props.match.params.id}/pecas`)
+    axios.get(`http://private-31df06-mockprojectcar.apiary-mock.com/carros/${this.props.match.params.id}/pecas`)
     .then((response) => {
       this.setState({ listaPecas: response.data });
     })
@@ -21,26 +21,43 @@ class Pecas extends Component {
     const copiaListaPecas = [];
 
     for(let index in this.state.listaPecas){
-      if(this.state.listaPecas[index].alerta === false)
+      if(this.state.listaPecas[index].alerta === "none")
       {
-      copiaListaPecas.push(
-      <div key={index} className="rectangle"> 
-        <div>{`Peça: ${this.state.listaPecas[index].nome}`}</div>
-        <div>{`Vida útil: ${this.state.listaPecas[index].km} km`}</div>
-        <div>{`Km atual: ${this.state.listaPecas[index].km_atual} km`}</div>
-      </div>)
+        copiaListaPecas.push(
+        <div key={index} className="rectangle"> 
+          <div><b>{`Peça:`}</b> {`${this.state.listaPecas[index].nome}`}</div>
+          <div><b>{`Km vida útil:`}</b> {`${this.state.listaPecas[index].km} km`}</div>
+          <div><b>{`Km atual:`}</b> {`${this.state.listaPecas[index].km_atual} km`}</div>
+          <div><b>{`${this.state.listaPecas[index].descricao} até:`}</b> {`${this.state.listaPecas[index].aceito}`}</div>
+          <div><b>{`${this.state.listaPecas[index].descricao} atual:`}</b> {`${this.state.listaPecas[index].atual}`}</div>
+        </div>)
+      }else if(this.state.listaPecas[index].alerta === "warning"){
+        copiaListaPecas.push(
+          <div key={index} className="rectangle"> 
+            <div><b>{`Peça:`}</b> {`${this.state.listaPecas[index].nome}`}</div>
+            <div><b>{`Km vida útil:`}</b> {`${this.state.listaPecas[index].km} km`}</div>
+            <div><b>{`Km atual:`}</b> {`${this.state.listaPecas[index].km_atual} km`}</div>
+            <div><b>{`${this.state.listaPecas[index].descricao} até:`}</b> {`${this.state.listaPecas[index].aceito}`}</div>
+            <div><b>{`${this.state.listaPecas[index].descricao} atual:`}</b> {`${this.state.listaPecas[index].atual}`}</div>
+            <div className="fundo-aviso-yellow">
+              <i className="alerta-yellow fas fa-exclamation-triangle fa-2x"></i> 
+              <div><i>Logo precisará ser revisada</i></div>
+            </div>
+          </div>)
       }else{
         copiaListaPecas.push(
           <div key={index} className="rectangle"> 
-            <div>{`Peça: ${this.state.listaPecas[index].nome}`}</div>
-            <div>{`Vida útil: ${this.state.listaPecas[index].km} km`}</div>
-            <div>{`Km atual: ${this.state.listaPecas[index].km_atual} km`}</div>
-            <div className="fundo-aviso">
-              <i className="alerta fas fa-exclamation-triangle fa-2x"></i> 
-              <div>Precisa ser trocada</div>
+            <div><b>{`Peça:`}</b> {`${this.state.listaPecas[index].nome}`}</div>
+            <div><b>{`Km vida útil:`}</b> {`${this.state.listaPecas[index].km} km`}</div>
+            <div><b>{`Km atual:`}</b> {`${this.state.listaPecas[index].km_atual} km`}</div>
+            <div><b>{`${this.state.listaPecas[index].descricao} até:`}</b> {`${this.state.listaPecas[index].aceito}`}</div>
+            <div><b>{`${this.state.listaPecas[index].descricao} atual:`}</b> {`${this.state.listaPecas[index].atual}`}</div>
+            <div className="fundo-aviso-red">
+              <i className="alerta-red fas fa-exclamation-triangle fa-2x"></i> 
+              <div><i>Precisa ser trocada</i></div>
             </div>
           </div>)
-      } 
+      }
     }
 
     return copiaListaPecas;
